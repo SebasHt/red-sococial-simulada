a26218419cbcf7c60371b592c8326f7efb1d7e37
johan sebastian henaotilano <henaotilanosebas@gmail.com>
2026-04-18 15:51:31 -0500

    se creom l a nav doavia no es funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Posst from './components/posst/Posst'
+import Navbars from './components/posst/navbars'  
+
 
 function App() {
   return (
     <div>
+      <Navbars />
       <Posst />
     </div>
   )
---
+++ src/components/posst/navbars.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+export default function Navbars() {
+  return (
+    <div class="navbar">
+        <ul class="navbarLista">
+            <li class="listas">Mensajes</li>
+            <li class="listas">Notificaciones</li>
+            <li class="listas-A">Cuenta</li>
+        </ul>
+
+    </div>
+  )
+}
+
