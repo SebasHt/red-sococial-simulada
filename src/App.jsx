03768055fb6f6e4f690dab983e8512b5b183c855
johan sebastian henaotilano <henaotilanosebas@gmail.com>
2026-04-20 14:17:29 -0500

    se adelanto bastante

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Posst from './components/posst/Posst'
-import Navbars from './components/posst/navbars'  
+import Navbars from './components/posst/navbars' 
+import crearPost from './components/posst/crearPost' 
 
 
 function App() {
   return (
     <div>
+      <crearPost/>
       <Navbars />
       <Posst />
     </div>
---
+++ src/components/posst/miniInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function miniInfo() {
+  return (
+    <div>
+        <img src="" alt="" />
+        <samp></samp>
+        
+    </div>
+  )
+}
